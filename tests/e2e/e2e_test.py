40108ddb80d0ff92bd7054a6155d203dd8a29db7
SirJousis <josiasgonzalez@outlook.com>
2026-03-06 13:40:15 +0100

    cambios selenium

--- tests/e2e/e2e_test.py
+++ tests/e2e/e2e_test.py
@@ -6,62 +6,64 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-URL = "http://10.227.87.81:6101"
 
-options = Options()
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+def test_e2e():
 
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()),
-    options=options
-)
+    URL = "http://10.227.87.81:6101"
 
-wait = WebDriverWait(driver, 10)
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-try:
-
-    driver.get(URL)
-
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Crear cuenta')]"))
-    ).click()
-
-    username = wait.until(
-    EC.element_to_be_clickable((By.ID, "username_register"))
-    )
-    username.send_keys("testuser")
-
-    driver.find_element(By.ID, "password_register").send_keys("testpass")
-
-    driver.find_element(
-        By.XPATH, "//button[contains(text(),'Registrarse')]"
-    ).click()
-
-    wait.until(
-        EC.element_to_be_clickable((By.ID, "username"))
-    ).send_keys("testuser")
-
-    driver.find_element(By.ID, "password").send_keys("testpass")
-
-    driver.find_element(
-    By.XPATH, "//button[contains(text(),'Entrar')]"
-    ).click()
-
-    dias = wait.until(
-        EC.visibility_of_element_located((By.ID, "dias"))
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
     )
 
-    driver.execute_script("arguments[0].scrollIntoView(true);", dias)
+    wait = WebDriverWait(driver, 10)
 
-    dias.clear()
-    dias.send_keys("3")
+    try:
 
-    driver.find_element(
-        By.XPATH, "//button[contains(text(),'Buscar rutinas')]"
-    ).click()
-    print("E2E TEST PASSED")
+        driver.get(URL)
 
-finally:
-    driver.quit()
+        wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Crear cuenta')]"))
+        ).click()
+
+        username = wait.until(
+            EC.element_to_be_clickable((By.ID, "username_register"))
+        )
+        username.send_keys("testuser")
+
+        driver.find_element(By.ID, "password_register").send_keys("testpass")
+
+        driver.find_element(
+            By.XPATH, "//button[contains(text(),'Registrarse')]"
+        ).click()
+
+        wait.until(
+            EC.element_to_be_clickable((By.ID, "username"))
+        ).send_keys("testuser")
+
+        driver.find_element(By.ID, "password").send_keys("testpass")
+
+        driver.find_element(
+            By.XPATH, "//button[contains(text(),'Entrar')]"
+        ).click()
+
+        dias = wait.until(
+            EC.visibility_of_element_located((By.ID, "dias"))
+        )
+
+        driver.execute_script("arguments[0].scrollIntoView(true);", dias)
+
+        dias.clear()
+        dias.send_keys("3")
+
+        driver.find_element(
+            By.XPATH, "//button[contains(text(),'Buscar rutinas')]"
+        ).click()
+
+    finally:
+        driver.quit()
